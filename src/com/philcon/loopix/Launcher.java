0ab0d26baf26ec18fd63cc59ecf2d6be3f2d5662
Conriano <julianeinspenner@yahoo.de>
2017-11-09 18:16:35 +0100

    The party begins

---
+++ src/com/philcon/loopix/Launcher.java
@@ -0,0 +1,15 @@
+package com.philcon.loopix;
+
+/**
+ * Loopix - Loop your picture!
+ * @author Philpsc / Conriano
+ * @version 0.1
+ */
+public class Launcher {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
